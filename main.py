3ce0f8e6e99d88bde793ee1fd2ade7bacc70ae02
Valery <valerysalinascruz@gmail.com>
2026-03-10 17:49:35 -0600

    revision final espero de main

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
 import sys
 
+#limpia cada valor individual de la cadena
 def limpiar_valor(valor):
-    
+
     valor = valor.strip()
-    caracteres_validos = "0123456789.-"
-    resultado = ""
+    caracteres_validos = "0123456789.-" #los caracteres validos son:numeros,punto decimal,negativos
+    resultado = "" #se guarda el numero limpio
 
     for char in valor:
-        if char in caracteres_validos:
-            resultado += char
+        if char in caracteres_validos: #verifica si el valor es valido
+            resultado += char #si es valido se lo agrega a resultado
 
     return resultado
 
-
+#procesa la cadena que se ingreso
 def procesar_linea(linea):
 
     linea = linea.strip()
 
-    if linea == "":
+    if linea == "": #si la cadena es vacia
         return 0
 
-    valores = linea.split(',')
-    suma = 0
+    valores = linea.split(',') #divide la cadena con comas
+    suma = 0  
 
-    for v in valores:
+    for v in valores: #recorre cada valor cuando los separa la coma
 
         limpio = limpiar_valor(v)
 
-        if limpio == "":
-            continue
+        if limpio == "": #si esta vacio depues de limpiar
+            continue #continua con el siguiente valor
 
         try:
             numero = float(limpio)
             suma += int(numero)  # truncar
-        except ValueError:
-            continue
+        except ValueError: #si ocurre un error al limpiarlo
+            continue 
 
     return suma
 
@@ -43,9 +44,8 @@ def main():
 
     while True:
         try:
-            print(f"Presione Ctrl Z para salir")
             pedircad = input("Ingrese la cadena SEPARANDO con comas los valores: \n")
-        except EOFError:
+        except EOFError: #Error si no hay entrada
             break
 
         if pedircad == '':
@@ -54,6 +54,7 @@ def main():
         resultadosuma = procesar_linea(pedircad)
         print(f"Resultado de la suma de la cadena: \n",resultadosuma)
 
-
+#Verifica que el archivo este ejecutando 
+#Ejecuta la funcion principal
 if __name__ == "__main__":
     main()
