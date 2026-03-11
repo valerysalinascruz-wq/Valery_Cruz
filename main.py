01d4f77e29114c42b191e1cb2604882725d856a6
Valery <valerysalinascruz@gmail.com>
2026-03-10 19:19:39 -0600

    TAREA RETO 1

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
 
     while True:
         try:
-            pedircad = input("La cadena SEPARANDO con comas los valores: \n")
+            pedircad = input("Ingrese la cadena SEPARANDO con comas los valores: \n")
         except EOFError: #Error si no hay entrada
             break
 
