7bf15a5b9eb9c5ce18cac5e9d9c3113b85d33576
Valery <valerysalinascruz@gmail.com>
2026-03-20 21:58:40 -0600

    codigo en pruebas

--- main.py
+++ main.py
@@ -24,39 +24,47 @@ def clasf_temp(temp):
      
 def main():
 
-    '''
+    
     # Encabezado
-    '''
+    
     print("ciudad,temperatura_celsius,clasificacion")
     primera_linea = True
     for linea in sys.stdin:
         linea = linea.strip()
+
+    #saltar encabezado    
         if primera_linea:
             primera_linea = False
             continue
-    print("PROCESANDO: ",linea)
-    
-    '''
 
+    # Saltar lineas vacias
+        if not linea:
+            continue
+        datos = linea.split(',')
 
-        # Saltar lineas vacias
+    # Validad que tenga 3 columnas
+        if len(datos) != 3:
+            continue
+        ciudad = datos[0].strip()
+        unidad = datos[2].strip().upper()
 
+    # Validar unidad
+        if unidad not in ['C', 'F']:
+            continue
+
+    # Convertir temperatura
+        try:
+            temp = float(datos[1])
+        except ValueError:
+            continue 
         
-        # Separar campos
+    # Convertir a Celsius
+        temp_cel = convertir_a_Cel(temp,unidad)
 
-        
-        # Validar unidad
-
-        # Convertir temperatura
-
-             # Ignorar si no es numero
-        
-        # Convertir a Celsius
-
-        
-        # Clasificar y imprimir
+    # Clasificar y imprimir
+        estado_de_cli= clasf_temp(temp_cel)
+        print(f"{ciudad},{temp_cel:.1f},{estado_de_cli}")
  
-'''
 
 if __name__ == "__main__":
     main()
