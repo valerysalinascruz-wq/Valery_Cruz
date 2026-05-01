84ac75a198d17cf2d40e9e015163b2f1cd5897a9
Valery <valerysalinascruz@gmail.com>
2026-05-01 14:18:55 -0600

    correcciones

--- reto6/main.py
+++ reto6/main.py
@@ -125,7 +125,7 @@ def validar_codigo(codigo: str) -> Dict:
         resultado["tipo"] = "factura"
         res = validar_factura(codigo)
 
-    elif re.match(r'^[A-Z]{3}-',codigo):
+    elif re.match(r'^[A-Z]{3}-\d{4}-[A-Z]{2}$',codigo):
         resultado["tipo"] = "producto"
         res = validar_producto(codigo)
 
